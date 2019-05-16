a21dd07d79ece0b32264e4dd2e51225a579cf567
KimsPractice <38434350+KimsPractice@users.noreply.github.com>
2019-05-16 17:33:22 +0900

    컴포넌트, 리스트 테스트
    컴포넌트 생성, 리스트생성, 타입체크 테스트

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Movie from "./Movie";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const movies = [
+  {
+    id: 1,
+    title: "Avangers : Assemble",
+    poster:
+      "https://images-na.ssl-images-amazon.com/images/I/71%2BNoq4xpNL._SY679_.jpg"
+  },
+  {
+    id: 2,
+    title: "Avangers : Age of Ultron",
+    poster:
+      "https://images-na.ssl-images-amazon.com/images/I/71SIYxcSNOL._SY606_.jpg"
+  },
+  {
+    id: 3,
+    title: "Avangers : Infinity War",
+    poster:
+      "https://images-na.ssl-images-amazon.com/images/I/A1krsAMZ4qL._SL1500_.jpg"
+  },
+  {
+    title: "Avangers : End Game",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg"
+  }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {movies.map((movie, index) => {
+          return (
+            <Movie title={movie.title} poster={movie.poster} key={index} />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
