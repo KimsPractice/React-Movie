e38c8a52950f9f5eb8cebc5e5299203728aa61ed
KimsPractice <38434350+KimsPractice@users.noreply.github.com>
2019-05-18 04:22:37 +0900

    state, function컴포넌트 테스트
    state, setstate로 데이터 추가하기,
    function컴포넌트 사용해보기

--- src/App.js
+++ src/App.js
@@ -2,41 +2,54 @@ import React, { Component } from "react";
 import Movie from "./Movie";
 import "./App.css";
 
-const movies = [
-  {
-    id: 1,
-    title: "Avangers : Assemble",
-    poster:
-      "https://images-na.ssl-images-amazon.com/images/I/71%2BNoq4xpNL._SY679_.jpg"
-  },
-  {
-    id: 2,
-    title: "Avangers : Age of Ultron",
-    poster:
-      "https://images-na.ssl-images-amazon.com/images/I/71SIYxcSNOL._SY606_.jpg"
-  },
-  {
-    id: 3,
-    title: "Avangers : Infinity War",
-    poster:
-      "https://images-na.ssl-images-amazon.com/images/I/A1krsAMZ4qL._SL1500_.jpg"
-  },
-  {
-    title: "Avangers : End Game",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg"
-  }
-];
-
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+            title: "Avangers : Assemble",
+            poster:
+              "https://images-na.ssl-images-amazon.com/images/I/71%2BNoq4xpNL._SY679_.jpg"
+          },
+          {
+            title: "Avangers : Age of Ultron",
+            poster:
+              "https://images-na.ssl-images-amazon.com/images/I/71SIYxcSNOL._SY606_.jpg"
+          },
+          {
+            title: "Avangers : Infinity War",
+            poster:
+              "https://images-na.ssl-images-amazon.com/images/I/A1krsAMZ4qL._SL1500_.jpg"
+          },
+          {
+            title: "Avangers : End Game",
+            poster:
+              "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg"
+          },
+          {
+            title: "SPIDER-MAN: FAR FROM HOME",
+            poster:
+              "https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/spider-man-far-from-home-posters-2.jpeg"
+          }
+        ]
+      });
+    }, 5000);
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    });
+    return movies;
+  };
+
   render() {
     return (
       <div className="App">
-        {movies.map((movie, index) => {
-          return (
-            <Movie title={movie.title} poster={movie.poster} key={index} />
-          );
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading !!"}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -2,26 +2,29 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-class Movie extends Component {
-  static propTypes = {
-    title: PropTypes.string,
-    poster: PropTypes.string
-  };
-
-  render() {
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster} />
-        <h1>{this.props.title}</h1>
-      </div>
-    );
-  }
+function Movie({ title, poster }) {
+  return (
+    <div>
+      <MoviePoster poster={poster} />
+      <h1>{title}</h1>
+    </div>
+  );
 }
 
-class MoviePoster extends Component {
-  render() {
-    return <img src={this.props.poster} />;
-  }
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string
+};
+
+function MoviePoster({ poster }) {
+  return (
+    <div>
+      <img src={poster} alt="Movie Poster" />
+    </div>
+  );
 }
 
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
+};
 export default Movie;
