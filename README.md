015c607a7b3b249bcc4e00af74c4ed82552f5b82
KimsPractice <38434350+KimsPractice@users.noreply.github.com>
2019-06-11 03:48:02 +0900

    react-route set

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 #무비 앱 클론
 
-react 기초 학습을 위해 CRA를 사용하여 무비 맵 클론
+React 학습을 위해 CRA를 사용하여 무비 맵 클론
+
+## Screens
+
+- [ ] Home
+- [ ] TV Shows
+- [ ] Search
+- [ ] Detail
---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Router from "./Router";
+import Header from "./Header";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Router />
+      </>
+    );
+  }
+}
+
+export default App;
