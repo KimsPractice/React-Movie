bfb6822774af5a7dd6a05f10804b955e3247d925
KimsPractice <38434350+KimsPractice@users.noreply.github.com>
2019-06-13 16:34:47 +0900

    style-components 적용

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import Router from "./Router";
-import Header from "./Header";
+import React, { Component } from "react";
+import Router from "Components/Router";
+import GlobalStyle from "Components/GlobalStyles";
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <>
-        <Header />
         <Router />
+        <GlobalStyle />
       </>
     );
   }
