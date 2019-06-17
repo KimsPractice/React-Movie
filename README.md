7fdc9a71bd9619c669d6436d974ae5a0340608da
KimsPractice <38434350+KimsPractice@users.noreply.github.com>
2019-06-18 08:15:36 +0900

    api 연동완료
    axios로 themoviedb api 연동

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ React 학습을 위해 CRA를 사용하여 무비 맵 클론
 - [ ] TV Shows
 - [ ] Search
 - [ ] Detail
+
+## API Verbs
+
+- [x] Now playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV)
+- [x] Popular (TV,Movie)
+- [x] Airing Today (TV)
+
+- [x] TV Show Detail
+- [x] Movie Detail
+- [x] Seaarch
---
+++ src/api.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "10f3f155117233d51c763629a6f3593a",
+    language: "ko"
+  }
+});
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  tvDetail: id =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    }),
+  searcTv: term =>
+    api.get("search/rv", {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
+};
+
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  moiveDetail: id =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    }),
+  searchMovie: term =>
+    api.get("search/movie", {
+      params: {
+        query: encodeURIComponent(term)
+      }
+    })
+};
